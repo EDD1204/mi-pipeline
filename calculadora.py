3589b27b8d3da5e64d53c1f77cb12df69a34f148
EDD1204 <152231326+EDD1204@users.noreply.github.com>
2026-05-23 14:36:49 -0600

    ci: agregar workflow de linter con GitHub Actions

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# calculadora.py — Alumno: ICO 8vo semestre
+# Unidad 4, Actividad 3 — Pipeline CI/CD
+def sumar(a, b):
+    """Devuelve la suma de dos números."""
+    return a + b
+def restar(a, b):
+    """Devuelve la resta de dos números."""
+    return a - b
+def multiplicar(a, b):
+    """Devuelve la multiplicación."""
+    return a * b
+def dividir(a, b):
+    """Devuelve la división."""
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero")
+    return a / b
